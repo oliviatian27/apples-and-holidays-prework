834bae1ab4d8e37ee390efc7001207d3ec4e4ef0
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-24 01:58:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].values
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -55,7 +55,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |key,value|
+    value.each do |k,v|
+      puts key
 end
 
 def all_holidays_with_bbq(holiday_hash)
