f686fa88c7b69a32c18fe29c2b950b8875de46e4
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-24 02:15:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,6 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.
   holiday_hash.each do |key,value|
     value.each do |k,v|
       v.map{|x|x.capitalize!}
